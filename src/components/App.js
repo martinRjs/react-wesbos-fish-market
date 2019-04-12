3a5c685c56b0ee8bdd4885a2faf272b58deabeeb
martinRjs <marteen.3107@gmail.com>
2019-04-12 17:30:18 -0500

    add firebase database, delete and update handlers

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,40 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
-import Fish from './Fish';
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
+import base from '../base';
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {}
   };
+
+  componentDidMount() {
+    const { params } = this.props.match;
+    const localStorageRef = localStorage.getItem(params.storeId);
+
+    if (localStorageRef) {
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      })
+    }
+
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: 'fishes'
+    });
+  }
+
+  componentDidUpdate() {
+    const params = this.props.match.params;
+    localStorage.setItem(params.storeId, JSON.stringify(this.state.order));
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
 
   addFish = fish => {
     const fishes = { ...this.state.fishes };
@@ -19,6 +45,20 @@ class App extends React.Component {
     this.setState({
       fishes
     });
+  };
+
+  updateFish = (key, updatedFish) => {
+    const fishes = { ...this.state.fishes };
+    fishes[key] = updatedFish;
+
+    this.setState({ fishes });
+  };
+
+  deleteFish = (key) => {
+    const fishes = { ...this.state.fishes };
+    fishes[key] = null;
+
+    this.setState({ fishes });
   };
 
   loadSampleFishes = () => {
@@ -31,7 +71,15 @@ class App extends React.Component {
     order[key] = order[key] + 1 || 1;
 
     this.setState({ order });
+  };
+  
 
+  removeFromOrder = (key) => {
+    const order = { ...this.state.order };
+    
+    delete order[key];
+
+    this.setState({ order });
   };
 
   render() {
@@ -41,19 +89,24 @@ class App extends React.Component {
           <Header tagline="French Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish 
-                key={key} 
-                details={this.state.fishes[key]} 
-                index={key} 
+              <Fish
+                key={key}
+                details={this.state.fishes[key]}
+                index={key}
                 addToOrder={this.addToOrder}
               />
             ))}
           </ul>
         </div>
-        <Order />
-        <Inventory 
-          addFish={this.addFish} 
-          loadSampleFishes={this.loadSampleFishes} 
+        <Order {...this.state} 
+          removeFromOrder={this.removeFromOrder}
+        />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+          fishes={this.state.fishes}
+          updateFish={this.updateFish}
+          deleteFish={this.deleteFish}
         />
       </div>
     );
