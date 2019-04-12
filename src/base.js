cab8731051a2c5ab5ded3c2b92bb28489ce0d75d
martinRjs <marteen.3107@gmail.com>
2019-04-12 17:37:31 -0500

    remove api key

--- src/base.js
+++ src/base.js
@@ -1,9 +1,8 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
 
-
+//TODO: add api key
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDdUDf3sq0yWJkFean2OBdOhOR4VUO0TzU",
   authDomain: "catch-of-the-day-martin-rojas.firebaseapp.com",
   databaseURL: "https://catch-of-the-day-martin-rojas.firebaseio.com"
 });
